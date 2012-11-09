9b6aae7e03d7c7d982aa436f3bf56db1c0249a4a
Stepan Anchugov <kixxx1@gmail.com>
2012-11-09 07:39:51 +0600

    Rm un-needed .gitkeep's

--- spec/factories.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-
-  factory :page, :class => Page do
-    sequence(:title) {|n| "Page #{n}" }
-    sequence(:slug)  {|n| "page#{n}"}
-    content '**bold** \\\\italic\\\ stuff and ((/home links too))'
-  end
-
-end
